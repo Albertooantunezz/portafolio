08229953d71dc0b0a513c8666700df396f2947ea
Albertooantunezz <125560597+Albertooantunezz@users.noreply.github.com>
2025-05-29 20:39:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  safelist: [
+    'text-yellow-400',
+    'hover:text-yellow-400',
+    'bg-gray-800',
+    'text-white',
+    'text-4xl',
+    'text-2xl',
+    'rounded-2xl',
+    'p-5',
+    'gap-10',
+    'mt-10',
+    'w-20',
   ],
   theme: {
     extend: {},
